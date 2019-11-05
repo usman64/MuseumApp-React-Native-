302626045dd0e9801d964eba1cbd43e78501e51b
Ammar <ammar26497@gmail.com>
2019-11-05 18:34:32 +0500

    First Commit

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react'
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>This is the museum app!</Text>
+      </View>
+    )
+  }
 }
+
+export default App
+
 
 const styles = StyleSheet.create({
   container: {
