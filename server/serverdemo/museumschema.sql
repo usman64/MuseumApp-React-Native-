5b5e13f3dbd0742aa5cef31b51390b1acfaf11f1
Mohammad Obaid Ur Rehman <20100044@lums.edu.pk>
2019-11-16 16:10:28 +0500

    museum schema updated

--- server/serverdemo/museumschema.sql
+++ server/serverdemo/museumschema.sql
@@ -1,52 +1,70 @@
-CREATE TABLE puppets(
-    id NOT NULL AUTO_INCREMENT,
-    category VARCHAR(255), 
-    puppet_year YEAR,
-    region VARCHAR(255),
-    description MEDIUMTEXT,
-    PRIMARY KEY(id)
+-- USE testing1;
+CREATE SEQUENCE puppets_seq;
+
+CREATE TABLE puppets (
+	puppetId INT CHECK (puppetId > 0) DEFAULT NEXTVAL ('puppets_seq') NOT NULL PRIMARY KEY,
+	category VARCHAR(255),
+	puppetYear INTEGER,
+	region VARCHAR(255),
+	description TEXT
 );
 
-CREATE TABLE puppet_image(
-    id,
-    image_id,
-    image_link,
-    PRIMARY KEY(id)
+CREATE SEQUENCE puppetImage_seq;
+
+CREATE TABLE puppetImage (
+	imageId INT CHECK (imageId > 0) DEFAULT NEXTVAL ('puppetImage_seq') NOT NULL PRIMARY KEY,
+    imageLink VARCHAR(255),
+    puppetId INT CHECK (puppetId > 0),
+    FOREIGN KEY (puppetId) REFERENCES puppets(puppetId)
 );
 
-CREATE TABLE puppet_video(
-    id,
-    video_id,
-    video_link,
-    PRIMARY KEY(id)
+CREATE SEQUENCE puppetVideo_seq;
+
+CREATE TABLE puppetVideo (
+    videoId INT CHECK (videoId > 0) DEFAULT NEXTVAL ('puppetVideo_seq') NOT NULL PRIMARY KEY,
+    videoLink VARCHAR(255),
+    puppetId INT CHECK (puppetId > 0),
+    FOREIGN KEY (puppetId) REFERENCES puppets(puppetId)
+);
+-- -- -- -- -- -- -- -- -- -- --
+CREATE SEQUENCE events_seq;
+
+CREATE TABLE events (
+    eventId INT CHECK (eventId > 0) DEFAULT NEXTVAL ('events_seq') NOT NULL PRIMARY KEY,
+    eventDescription TEXT,
+    eventDate DATE,
+    eventTime TIME(0),
+    eventCategory VARCHAR(255), -- regular special 
+    poster VARCHAR(255) -- link
 );
 
-CREATE TABLE events(
-    id,
-    date,
-    time,
-    category,
-    poster
+CREATE TABLE relatedEvents(
+    eventId INT CHECK (eventId > 0),
+    FOREIGN KEY (eventId) REFERENCES events(eventId),
+    relatedEventId INT CHECK (relatedEventId > 0),-- inherits from events
+    PRIMARY KEY (eventId, relatedEventId)
 );
 
-CREATE TABLE related_events(
-    id,
-    related_event_id
+CREATE SEQUENCE eventImages_seq;
+
+CREATE TABLE eventImages (
+	imageId INT CHECK (imageId > 0) DEFAULT NEXTVAL ('eventImages_seq') NOT NULL PRIMARY KEY,-- don't need this
+    eventId INT CHECK (eventId > 0),
+    FOREIGN KEY (eventId) REFERENCES events(eventId),
+    imageLink VARCHAR(255)
 );
 
-CREATE TABLE event_photos(
-    id,
-    image_id,
-    image
-);
+CREATE SEQUENCE eventTrailer_seq;
 
-CREATE TABLE event_trailer(
-    id,
-    trailer_id
-    trailer
+CREATE TABLE eventTrailer (
+    trailerId INT CHECK (trailerId > 0) DEFAULT NEXTVAL ('eventTrailer_seq') NOT NULL PRIMARY KEY, --don't need this
+    eventId INT CHECK (eventId > 0),
+    FOREIGN KEY (eventId) REFERENCES events(eventId),
+    trailerLink VARCHAR(255)
 );
-
-CREATE TABLE maps(
-    floor_id,
-    image
+-- -- -- -- --
+CREATE TABLE maps (
+    floorId INT CHECK (floorId > 0),
+    imageLink VARCHAR(255),
+    PRIMARY KEY(floorId, imageLink)
 );
