88525b7dd8eb72003037b489715c418a02fc5ed6
Mohammad Obaid Ur Rehman <20100044@lums.edu.pk>
2019-11-18 03:22:02 +0500

    more and about pages made first iteration

--- components/About.js
+++ components/About.js
@@ -1,13 +1,42 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import  SinglePuppetScreen  from './SinglePuppetScreen'
+import { View, Text, StyleSheet, Header} from 'react-native';
 
 const About = () => {
   return (
-    <View>
-      <SinglePuppetScreen />
+//     <Header
+//   centerComponent={{ text: 'MY TITLE', style: { color: '#fff' } }}
+// />
+    <View style={styles.container}>
+      <Text style={styles.item}>Rafi Peer Cultural Centre is one of the most culturally 
+        rich places in Pakistan. Since it's inception the centre has hosted many national and continental events, 
+        such as The Asia Puppet Festival and the Literary Festival.
+        {"\n\n"}
+        The centre also has a multitude of attractions including the Rafi Peer Theatre and the Peeru's Cafe but arguably the most liked attraction is 
+        the Museum of Puppetry, which has the honor of being Asia's largest puppet museum and is home to puppets from more than 40 different 
+        countries.
+      </Text>
     </View>
   );
 };
+
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   padding: 20,
+  },
+  item: {
+    fontSize: 20,
+    textAlign: 'center',
+    lineHeight: 35,
+    // letterSpacing: 1
+  },
+  listitem: {
+    borderStyle: 'dashed',
+    borderBottomWidth: 3,
+    opacity: 1,
+    borderBottomColor: '#800000'
+  }
+})
 
 export default About;
--- containers/More.js
+++ containers/More.js
@@ -1,21 +1,92 @@
 import React, { Component } from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import { Text, View, TouchableOpacity, StyleSheet, FlatList, Switch  } from 'react-native';
+import SafeAreaView from 'react-native-safe-area-view';
 
 export class More extends Component {
+  constructor(){
+    super();
+    // const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    // this.state = {
+    //   dataSource: ds.cloneWithRows(['row 1', 'row 2']),
+    // };
+  }
+  navigator(where){
+    if (where === 'About'){
+      this.props.navigation.navigate('About')
+    }
+  }
+  state = {
+    isSwitchOn: false,
+  };
   render() {
+    const { isSwitchOn } = this.state;
     return (
-      <View>
-        <Text> More </Text>
-        <TouchableOpacity
-          onPress={() => this.props.navigation.navigate('About')}
-        >
-          <Text>Go To About</Text>
-        </TouchableOpacity>
+      <SafeAreaView style={{ flex: 1 }}>
+         <View style={{ flex: 1 }}>
+      <View style={styles.container}>
+        
+        <FlatList
+          data={[
+            {key: 'About'},
+            {key: 'Contact Us'},
+            {key: 'Find Us'},
+            {key: 'Language'},
+            {key: 'Settings'},
+            {key: 'Help'},
+          ]}
+          renderItem={
+            ({item}) =>{ 
+              if(item.key === "Language"){
+                return (
+                  <View
+                  style={{flex:1,
+                  flexDirection: 'row-reverse',
+                  justifyContent: 'space-between',}}
+                  onPress={() => this.navigator(item.key)}>
+                  <Switch
+                  // style={{justifyContent: 'flex-end',}}
+                  value={isSwitchOn}
+                  onValueChange={() =>
+                    { this.setState({ isSwitchOn: !isSwitchOn }); }
+                  }
+                  />                  
+                  <Text style={styles.item}>{item.key}
+                  </Text>
 
-      
+                  </View>
+                )
+              }
+              else{
+                return (
+                  <TouchableOpacity style={{flex:1}}
+                  // style={styles.listitem}
+                  onPress={() => this.navigator(item.key)}>
+                  <Text style={styles.item}>{item.key}</Text>
+                  </TouchableOpacity>
+                )
+              }
+            } 
+          }
+        />
       </View>
+      </View>
+      </SafeAreaView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   paddingTop: 20
+  },
+  item: {
+    marginLeft: 10,
+    padding: 10,
+    fontSize: 24,
+    height: 50,
+    justifyContent: 'flex-start',
+  }
+})
 
 export default More;
