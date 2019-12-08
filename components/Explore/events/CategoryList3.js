359cdbff7c4f19f3ebfd5286d7a6e3ad0d48e2b0
Ammar <ammar26497@gmail.com>
2019-12-09 01:52:48 +0500

    Event Card UI Done

--- components/Explore/events/CategoryList3.js
+++ components/Explore/events/CategoryList3.js
@@ -3,26 +3,67 @@ import { Text, View, ScrollView } from 'react-native';
 import Category3 from './Category3';
 
 class CategoryList3 extends Component {
+
+  checkmonth = month => {
+    month = Number(month)
+    if(month === 1) return 'JAN'
+    else if(month === 2) return 'FEB'
+    else if(month === 3) return 'MAR'
+    else if(month === 4) return 'APR'
+    else if(month === 5) return 'MAY'
+    else if(month === 6) return 'JUN'
+    else if(month === 7) return 'JUL'
+    else if(month === 8) return 'AUG'
+    else if(month === 9) return 'SEP'
+    else if(month === 10) return 'OCT'
+    else if(month === 11) return 'NOV'
+    else if(month === 12) return 'DEC'
+  }
+
+  changeTime = time => {
+    let hour = Number(time.substring(0,2));
+    let min = Number(time.substring(3,5));
+    let dn = '';
+
+    if(hour >=0 && hour < 12) dn='AM'
+    else {
+      dn='PM'
+      hour=hour-12; 
+    }
+
+    let thetime = `${hour.toString()}:${min.toString()} ${dn}`
+    return thetime;
+
+  }
+
   render() {
     return (
       <View style={{ height: 240, marginTop: 20 }}>
         <View>
-          {this.props.data.map((item, key) => (
-            <View key={key} style={{paddingTop: 20}}>
-            <Category3
-              id={item.eventid}
-              ImageUri={item.poster}
-              name={item.eventname}
-              day={item.eventdate.substring(5,7)}
-              month={item.eventdate.substring(8,10)}
-              date={item.eventdate.substring(0,10)}
-              time={item.eventtime}
-              description={item.eventdescription}
-              key={key}
-              navigation={this.props.navigation}
-            />
-            </View>
-          ))}
+          {this.props.data.map((item, key) => {
+            let month = item.eventdate.substring(5,7);
+            let themonth = this.checkmonth(month);
+
+            let time = item.eventtime;
+            let thetime = this.changeTime(time)
+            return (
+              <View key={key} style={{paddingTop: 20}}>
+              <Category3
+                id={item.eventid}
+                ImageUri={item.poster}
+                name={item.eventname}
+                day={item.eventdate.substring(8,10)}
+                month={themonth}
+                date={item.eventdate.substring(0,10)}
+                time={thetime}
+                description={item.eventdescription}
+                key={key}
+                navigation={this.props.navigation}
+              />
+              </View>
+
+            )
+      })}
         </View>
       </View>
     );
